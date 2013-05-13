54c0faffcc3bd4ea78a45b046f7867460a96883f
Christophe Simon <geektophe@gmail.com>
2013-05-13 23:36:43 +0200

    Added presentation agent interface and base class.

--- spark/interfaces.py
+++ spark/interfaces.py
@@ -56,11 +56,16 @@ class IControlAgent(Interface):
     def set_readonly(flag):
         """
         Sets component's read only state flag
+
+        @param flag:    The flag representing the read-only state (may be True
+                        or False)
         """
 
-    def set_presentation_agent(presentation_agent):
+    def set_presentation_agent(agent):
         """
         Sets presentation agent instance
+
+        @param agent:   The presentation agent instance to bind.
         """
 
     def get_presentation_agent():
@@ -68,9 +73,11 @@ class IControlAgent(Interface):
         Sets presentation agent instance
         """
 
-    def set_abstraction_agent(presentation_agent):
+    def set_abstraction_agent(agent):
         """
         Sets abstraction agent instance
+
+        @param agent:   The presentation agent instance to bind.
         """
 
     def get_abstraction_agent():
@@ -81,14 +88,105 @@ class IControlAgent(Interface):
     def get_value(name):
         """
         Returns attribute value identified by name
+
+        @param name:    The name identifying the attribute to get value from.
         """
 
     def set_value(name, value):
         """
         Sets attribute value identified by name
+
+        @param name:    The name identifying the attribute to set value to.
+        @param value:   The value to set.
         """
 
     def destroy():
         """
         Cleanly destroyes all components
+        """
+
+
+class IPresentationAgent(IEventBroker):
+    """
+    Presentation agent interface definition
+    """
+
+    def get_control_agent():
+        """
+        Returns control agent instance.
+        """
+
+    def get_toplevel():
+        """
+        Returns toplevel component
+        """
+
+    def attach_slave(name, container_name, slave):
+        """
+        Attach a sub (slave) presentation agent view.
+
+        @param name:            The slave name
+        @param container_name:  The contianer widget name to pack slave into.
+        @param slave:           The slave presentation instance to attach.
+        """
+
+    def enable(name):
+        """
+        Enables a widget (set it resopnsive to user actions).
+
+        @param name:    The name identifying the widget to enable.
+        """
+
+    def disable(name):
+        """
+        Disables a widget (set it inresopnsive to user actions).
+
+        @param name:    The name identifying the widget to disable.
+        """
+
+    def get_value(name):
+        """
+        Reads a widget value or state.
+
+        @param name:    The name identifying the widget to read value from.
+        """
+
+    def set_value(name, value):
+        """
+        Sets a control value or state.
+
+        @param name:    The name identifying the widget to set value to.
+        """
+
+    def set_isems(name, items):
+        """
+        Sets a list of available (selectable) items on list widgets.
+
+        @param name:    The name identifying the widget to set value to.
+        @param items:   A list containing the values to set.
+        """
+
+    def set_error(name, state, mesg=""):
+        """
+        Sets a widget in a state showing than an incorrect value has been set,
+        or set it back in normal state.
+
+        @param name:    The name identifying the widget to set error state.
+        @param state:   The error state to set (may be True or False)
+        @param mesg:    The error message to set in case of error.
+        """
+
+    def show():
+        """
+        Shows the top level window.
+        """
+
+    def hide():
+        """
+        Shows the top level window.
+        """
+
+    def destroy():
+        """
+        Cleanly destroyes component.
         """
---
+++ spark/presentation/gtk.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+"""
+Base view to be subclassed
+"""
+
+import pygtk
+import gtk
+import os
+from sparck.interfaces import IPresentationAgent
+from sparck.event import EventBus
+from sparck.agent import BaseAgent
+from zope.interface import implements
+
+pygtk.require("2.0")
+
+
+class GtkBasePresentationAgent(BaseAgent):
+    """
+    Main site add/edit/view interface.
+
+    The interface design is loaded from a glade file.
+    """
+    implements(IPresentationAgent)
+
+    GLADE_FILE = ""
+    TOPLEVEL_NAME = ""
+
+    def __init__(self, control_agent):
+        """
+        Basic view to be subclassed.
+        """
+        BaseAgent.__init__(self)
+
+        if not os.path.isfile(self.GLADE_FILE):
+            raise RuntimeError("No glade file found.")
+
+        self._control_agent = control_agent
+        self._builder = gtk.Builder()
+        self._builder.add_from_file(self.GLADE_FILE)
+        self._slaves = {}
+        self._attr_validity = {}
+
+    def __getitem__(self, name):
+        """
+        Returns widget named name
+        """
+        if self._builder.get_object(name) is not None:
+            return self._builder.get_object(name)
+
+        if name in self._slaves:
+            return self._slaves[name]
+
+        return None
+
+    def get_control_agent(self):
+        """
+        Returns control agent instance
+        """
+        return self._control_agent
+
+    def get_toplevel(self):
+        """
+        Returns toplevel component
+        """
+        return self[self.TOPLEVEL_NAME]
+
+    def attach_slave(self, name, container, slave):
+        """
+        Attach a slave view to the master view.
+        """
+        if not isinstance(slave, GtkBasePresentationAgent):
+            raise TypeError(
+                "slave must be a GtkBasePresentationAgent, not a %s" %
+                type(slave))
+
+        slave_toplevel = slave.get_toplevel()
+        widget = slave_toplevel.get_child()
+        slave_toplevel.remove(widget)
+        self.pack_slave(widget, container)
+        self._slaves[name] = slave
+
+    def pack_slave(self, widget, container):
+        """
+        Packs a slave window into the container identifed by container name.
+
+        If the default pack behaviour does not suit your needs, you may
+        override this method.
+
+        @param widget:      The slave widget to attach in container.
+        @param container:   The container widgte name to add slave to.
+        """
+        container = self._builder.get_object(container)
+
+        if container is None:
+            raise AttributeError("No container named %s" % container)
+
+        container.pack_start(widget)
+
+    def get_objects(self):
+        """
+        Returns all widgets
+        """
+        return self._builder.get_objects()
+
+    def set_combobox_items(self, widget, items):
+        """
+        Sets an interface combobox items. Each item is composed of a mnemonic
+        name and a label, but only the label is displayed in the combobox.
+        """
+        # The model is configured for rows composed of name and a label
+        model = gtk.ListStore(str, str)
+        widget.set_model(model)
+        # A text rederer is used to display rows
+        renderer = gtk.CellRendererText()
+        widget.pack_start(renderer, True)
+        # Only the row's label is displayed (second element of the tupple: 1)
+        widget.add_attribute(renderer, 'text', 1)
+
+        # Appends items to the combobox
+        names = items.keys()
+        names.sort()
+
+        for name in names:
+            model.append((name, items[name]))
+
+        if names:
+            widget.set_active(0)
+
+    def get_combobox_selection(self, widget):
+        """
+        Retrieves the value of the list selected item.
+        """
+        model = widget.get_model()
+        index = widget.get_active()
+
+        if index < 0:
+            return None
+
+        return model[index][0]
+
+    def set_list_items(self, widget, items):
+        """
+        Sets an interface combobox items. Each item is composed of a mnemonic
+        name and a label, but only the label is displayed in the combobox.
+
+        Items should be a list of tuples corresponding to tho number of items
+        in the list moldel.
+        """
+        model = widget.get_model()
+        model.clear()
+
+        for item in items:
+            model.append(item)
+
+    def get_list_selection(self, widget):
+        """
+        Retrieves the value of the list selected item.
+        """
+        model, rows = widget.get_selection().get_selected_rows()
+        return rows
+
+    def set_list_selection(self, widget, value):
+        """
+        Selects the list row which name column equals value.
+        """
+        model = widget.get_model()
+        index = 0
+
+        for row in model:
+            if row[0] == value:
+                break
+            index += 1
+
+        if index < len(model):
+            widget.set_active(index)
+        else:
+            widget.set_active(-1)
+
+    def show(self):
+        """
+        Shows window
+        """
+        self.get_toplevel().show()
+
+    def destroy(self):
+        """
+        Cleanly destroyes components
+        """
+        # Destroyes slaves if it has some
+        for name, slave in self._slaves.items():
+            slave.destroy()
+            del self._slaves[name]
+
+        # Clears event bus observers
+        self.get_event_bus().unsubscribe_all()
+        self.get_toplevel().destroy()
+
+    def enable(self, name):
+        """
+        Enables a control (set it resopnsive to user actions)
+        """
+        self[name].set_sensitive(True)
+
+    def disable(self, name):
+        """
+        Disables a control (set it unresopnsive to user actions)
+        """
+        self[name].set_sensitive(False)
+
+    def set_enabled(self, name, state):
+        """
+        Enables or disables a control based on state value
+        """
+        self[name].set_sensitive(state)
+
+    def set_items(self, name, items):
+        """
+        Sets a finite value selection control (ex. list or combo box)
+        selectable items.
+        """
+        widget = self[name]
+
+        if isinstance(widget, gtk.List) or isinstance(widget, gtk.TreeView):
+            self.set_list_items(widget, items)
+        elif isinstance(widget, gtk.ComboBox):
+            self.set_combobox_items(widget, items)
+        else:
+            raise NotImplementedError('Unhandled control type for %s: %s' % \
+                (name, widget.__class__.__name__))
+
+    def get_value(self, name):
+        """
+        Reads a control value or state.
+        """
+        widget = self[name]
+
+        if isinstance(widget, gtk.Entry):
+            return widget.get_text()
+        elif isinstance(widget, gtk.ToggleButton):
+            return widget.get_active()
+        elif isinstance(widget, gtk.ComboBox):
+            return self.get_combobox_selection(widget)
+        elif isinstance(widget, gtk.List) or isinstance(widget, gtk.TreeView):
+            return self.get_list_selection(widget)
+        else:
+            raise NotImplementedError('Unhandled control type for %s: %s' % \
+                (name, widget.__class__.__name__))
+
+    def set_value(self, name, value):
+        """
+        Sets a control value or state.
+        """
+        widget = self[name]
+
+        if isinstance(widget, gtk.Entry):
+            widget.set_text(value)
+        elif isinstance(widget, gtk.ToggleButton):
+            widget.set_active(value)
+        elif isinstance(widget, gtk.List) or isinstance(widget, gtk.ComboBox):
+            self.set_list_selection(widget, value)
+        else:
+            raise NotImplementedError('Unhandled control type for %s: %s' % \
+                (name, widget.__class__.__name__))
+
+    def set_error(self, name, state, mesg=""):
+        """
+        Sets a crontrol in a state showing than an error occured or that all
+        is correct.
+
+        An optional error message to be displayed may be passed.
+        """
+        widget = self[name]
+
+        if state:
+            widget.modify_base(gtk.STATE_NORMAL, gtk.gdk.color_parse('#FFCCCC'))
+            widget.set_tooltip_text(mesg)
+        else:
+            widget.set_tooltip_text('')
+            widget.modify_base(gtk.STATE_NORMAL, gtk.gdk.color_parse('#90EE90'))
